f4d9f597f936c9d39434978a5d0ee30800293bd0
Дмитрий Яровой <yarovoy.dmytro@gmail.com>
2020-06-30 15:23:47 +0300

    done

---
+++ src/container/testPage/testPage.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import axios from 'axios';
+import * as _ from 'lodash';
+import Select from 'react-select';
+
+import './testPage.css';
+
+const requestsData = [
+    { requestURL: 'https://beta.autobooking.com/api/test/v1/search/terms', label: 'terms' },
+    { requestURL: 'https://beta.autobooking.com/api/test/v1/search/styles', label: 'styles' },
+    { requestURL: 'https://beta.autobooking.com/api/test/v1/search/brands_terms', label: 'brandsTerms' },
+]
+
+let data = null
+
+class TestPage extends React.Component {
+    state = {
+        terms: [],
+        styles: [],
+        brandsTerms: [],
+        termsSelected: null,
+        stylesSelected: null,
+        brandsTermsSelected: null,
+        termsURL: '',
+        stylesURL: '',
+        brandsTermsURL: ''
+    }
+
+    componentDidMount() {
+        _.map(requestsData, item => {
+            axios.get(item.requestURL)
+                .then((response) => {
+                    this.setState({
+                        [item.label]: response.data,
+                    })
+                })
+                .catch((error) => {
+                    // handle error
+                })
+                .finally(() => {
+                    // always executed
+                });
+        })
+        _.map(this.props.match.params, (param, key) => {
+            return this.setState({
+                [key + 'Selected']: _.split(param, _.split(this.pref([key + 'Selected']), '/')[1])[1],
+                [key + "URL"]: '/' + param
+            })
+        })
+        _.map(data, (item, key) => {
+            this.setState({
+                [key]: item.value
+            })
+        })
+    }
+
+    pref = (currentDropdown) => {
+        if (currentDropdown[0] === 'termsSelected') {
+            return '/s-'
+        }
+        if (currentDropdown[0] === 'brandsTermsSelected') {
+            return '/b-'
+        }
+        if (currentDropdown[0] === 'stylesSelected') {
+            return '/st-'
+        }
+    }
+
+    onChange = (currentDropdown) => (e) => {
+        const splitName = _.split(currentDropdown, 'Selected', 1)
+        this.setState({
+            [currentDropdown]: e.value,
+            [splitName + "URL"]: this.pref(currentDropdown) + e.value
+        }, () => {
+            const url = this.state.termsURL + this.state.stylesURL + this.state.brandsTermsURL
+            this.props.history.push('')
+            this.props.history.push(url)
+        })
+    }
+
+    renderDropdown() {
+        if (!this.state.brandsTerms || !this.state.terms || !this.state.styles) { return null }
+        return _.map(requestsData, (item, key) => {
+            const checkCurrentValue = _.filter(this.state[item.label].data, selectItem => {
+                return selectItem.slug === this.state[item.label + 'Selected']
+            })
+            const options = _.map(this.state[item.label].data, item => {
+                return { value: item.slug, label: item.label }
+            })
+            if (!checkCurrentValue[0] && this.state[item.label + 'Selected']) { return <div key={key}>Loading...</div> }
+            const label = this.state[item.label + 'Selected'] ? checkCurrentValue[0].label : ''
+            const value = this.state[item.label + 'Selected'] ? checkCurrentValue[0].slug : ''
+            return <Select
+                key={key}
+                defaultValue={{ label, value }}
+                onChange={this.onChange([item.label + 'Selected'])}
+                options={options}
+            />
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderDropdown()}
+            </div>
+        )
+    }
+}
+
+export default TestPage;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Router } from 'react-router-dom';
+import history from './history';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={history}>
+      <App />
+  </Router>,
   document.getElementById('root')
 );
 
