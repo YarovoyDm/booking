7105977aa8e4bcfc35668d13bf68c00b17a16efa
Дмитрий Яровой <yarovoy.dmytro@gmail.com>
2020-07-02 13:48:07 +0300

    add parse

--- src/container/testPage/testPage.js
+++ src/container/testPage/testPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as _ from 'lodash';
 import Select from 'react-select';
+import axios from 'axios'
 
 import './testPage.css';
 import { API } from '../../utils';
@@ -10,8 +11,6 @@ const requestsData = [
     { requestURL: '/styles', label: 'styles' },
     { requestURL: '/brands_terms', label: 'brandsTerms' },
 ]
-
-let data = null
 
 class TestPage extends React.Component {
     state = {
@@ -23,21 +22,46 @@ class TestPage extends React.Component {
         brandsTermsSelected: null,
         termsURL: '',
         stylesURL: '',
-        brandsTermsURL: ''
+        brandsTermsURL: '',
+        parseData: null
     }
 
     componentDidMount() {
+        const allParams = [this.props.match.params.terms, this.props.match.params.styles, this.props.match.params.brandsTerms]
+        const styleForParse = _.filter(allParams, item => {
+            if(typeof item === 'string'){
+                return _.startsWith(item, 'st-');
+            }
+        })[0]
+        const brandForParse = _.filter(allParams, item => {
+            if(typeof item === 'string'){
+                return _.startsWith(item, 'b-');
+            }
+        })[0]
+        const termsForParse = _.filter(allParams, item => {
+            if(typeof item === 'string'){
+                return _.startsWith(item, 's-');
+            }
+        })[0]
+        axios.get('https://beta.autobooking.com/api/test/v1/search/parse_link', {
+            params: {
+                service_slug: _.split(termsForParse, _.split(this.pref(['termsSelected']), '/')[1])[1],
+                brand_slug: _.split(brandForParse, _.split(this.pref(['brandsTermsSelected']), '/')[1])[1],
+                style_slug: _.split(styleForParse, _.split(this.pref(['stylesSelected']), '/')[1])[1]
+            }
+        }).then(res => {
+            this.setState({
+                termsURL:  termsForParse ? '/' + termsForParse : '',
+                stylesURL:  styleForParse ? '/' + styleForParse : '',
+                brandsTermsURL:  brandForParse ? '/'+ brandForParse : '',
+                parseData: res.data
+            })
+        })
         _.map(requestsData, item => {
             API(item.requestURL).then((response) => {
                 this.setState({
                     [item.label]: response.data,
                 })
-            })
-        })
-        _.map(this.props.match.params, (param, key) => {
-            return this.setState({
-                [key + 'Selected']: _.split(param, _.split(this.pref([key + 'Selected']), '/')[1])[1],
-                [key + "URL"]: '/' + param
             })
         })
     }
@@ -62,24 +86,31 @@ class TestPage extends React.Component {
         }, () => {
             const url = this.state.termsURL + this.state.stylesURL + this.state.brandsTermsURL
             this.props.history.push('')
-            this.props.history.push(url)
+            this.props.history.push(url) 
         })
     }
 
     renderDropdown() {
-        const {brandsTerms, styles, terms} = this.state
+        const { brandsTerms, styles, terms } = this.state
+        const rename = {
+            brandsTerms: 'brand',
+            styles: 'style',
+            terms: 'service'
+        }
         if (!brandsTerms || !terms || !styles) { return null }
         return _.map(requestsData, (item, key) => {
-            const selected  = this.state[item.label + 'Selected']
-            const checkCurrentValue = _.filter(this.state[item.label].data, selectItem => {
+            const selected = this.state[item.label + 'Selected']
+            const currentValue = _.filter(this.state[item.label].data, selectItem => {
                 return selectItem.slug === selected
             })
+            if(!this.state.parseData){return null}
+            const currentValueParse = this.state.parseData[rename[item.label]]
             const options = _.map(this.state[item.label].data, item => {
                 return { value: item.slug, label: item.label }
             })
-            if (!checkCurrentValue[0] && selected) { return <div key={key}>Loading...</div> }
-            const label = selected ? checkCurrentValue[0].label : ''
-            const value = selected ? checkCurrentValue[0].slug : ''
+            if (!currentValue[0] && selected) { return <div key={key}>Loading...</div> }
+            const label = this.state.parseData ? currentValueParse.label : selected ? currentValue[0].label : ''
+            const value = this.state.parseData ? currentValueParse.label : selected ? currentValue[0].slug : ''
             return <Select
                 key={key}
                 defaultValue={{ label, value }}
